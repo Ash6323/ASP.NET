449b682c8dde8c8f64611f87ff3dac4cd79e6bf5
Ash6323 <ashwin6323@gmail.com>
2023-04-23 10:40:39 +0530

    Added Lexicon Project

---
+++ Lexicon/Lexicon.Data/Models/Invoice.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Lexicon.Data.Models
+{
+    public class Invoice
+    {
+        public int Id { get; set; }
+        public DateOnly Date { get; set; }
+        public int HoursWorked { get; set; }
+        public float? TotalAmount { get; set; }
+    }
+}
---
+++ Lexicon/Lexicon.Data/Models/Jusrisdiction.cs
@@ -0,0 +1,12 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace Lexicon.Data.Models
+{
+    public class Jusrisdiction
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Area { get; set; }
+    }
+}
